61a62efe2c1b1a0b2aa4a55f0a265025132e753c
Łukasz <lukstasiak26@wp.pl>
2015-11-08 18:36:06 +0100

    hihoh

--- test2/src/controller/Main.java
+++ test2/src/controller/Main.java
@@ -20,7 +20,6 @@ public class Main extends Application {
 		
 	}
 	public void mainWindow(){
-		
 		try {
 			FXMLLoader loader = new FXMLLoader(Main.class.getResource("/view/MainWindowView.fxml"));
 			AnchorPane pane = loader.load();
